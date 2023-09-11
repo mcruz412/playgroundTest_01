b807d0a550c26dfa229cc424f0ca6d4e822a7e2a
mcruz412 <mcruz.bma12@gmail.com>
2023-09-11 11:32:58 -0400

    added driveTerrain as property

--- functionsForFunSwift.playground/Contents.swift
+++ functionsForFunSwift.playground/Contents.swift
@@ -7,9 +7,18 @@ struct VehicleInformation {
     let LicensePlate: String
 }
 
+enum DriveTrain {
+    case rwd = "Rear-Wheel Drive"
+    case fwd = "Front-Wheel Drive"
+    case fourwd = "Four-Wheel Drive"
+    case awd = "All-Wheel Drive"
+}
 
 struct Vehicle {
     let information: VehicleInformation
+    let driveTrain: DriveTrain
 }
 
-let fourRunner = Vehicle(information: .init(Make: "Toyota", Model: "4 Runner", Year: 2014, LicensePlate: "55AA22"))
+let fourRunner = Vehicle(information: .init(Make: "Toyota", Model: "4 Runner", Year: 2014, LicensePlate: "55AA22", driveTrain: .awd))
+
+
