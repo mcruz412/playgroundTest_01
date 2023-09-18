3c1a4f75a595c285185a1cbe01d3c96618db1dd9
mcruz412 <mcruz.bma12@gmail.com>
2023-09-18 08:15:30 -0600

    Create diiferent cars with different drivetrains

--- functionsForFunSwift.playground/Contents.swift
+++ functionsForFunSwift.playground/Contents.swift
@@ -8,9 +8,10 @@ struct VehicleInformation {
 }
 
 enum DriveTrain {
-    case rwd = "Rear-Wheel Drive"
-    case fwd = "Front-Wheel Drive"
-    case fourwd = "Four-Wheel Drive"
+    case rearWheelDrive
+    case frontWheelDrive
+    case fourWheelDrive
+    case allWheelDrive
     case awd = "All-Wheel Drive"
 }
 
@@ -19,6 +20,15 @@ struct Vehicle {
     let driveTrain: DriveTrain
 }
 
-let fourRunner = Vehicle(information: .init(Make: "Toyota", Model: "4 Runner", Year: 2014, LicensePlate: "55AA22", driveTrain: .awd))
+let fourRunner = Vehicle(information: .init(Make: "Toyota", Model: "4 Runner", Year: 2014, LicensePlate: "55AA22", driveTrain: .allWheelDrive))
 
+let corolla = Vehicle(information: .init(Make: "Toyota", Model: "Corolla", Year: 2021, LicensePlate: "ABC1EFG", driveTrain: .frontWheelDrive))
+
+let landCruiser = Vehicle(information: .init(Make: "Toyota", Model: "LandCruiser", Year: 2023, LicensePlate: "GR4NDM01", driveTrain: .awd))
+
+let deForester = Vehicle(information: .init(Make: "Subaru", Model: "DeForester", Year: 2021, LicensePlate: "YG3YBXD", driveTrain: .awd))
+
+//let carArray = [fourRunner, corolla]
+
+//print(carArray)
 
