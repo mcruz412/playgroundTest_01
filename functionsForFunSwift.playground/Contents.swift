5c2e9db1a684327c65398bb7fd99b7a4bb08a507
mcruz412 <mcruz.bma12@gmail.com>
2023-09-18 08:43:21 -0600

    removed driveTrain property outside of the .init

--- functionsForFunSwift.playground/Contents.swift
+++ functionsForFunSwift.playground/Contents.swift
@@ -12,7 +12,7 @@ enum DriveTrain {
     case frontWheelDrive
     case fourWheelDrive
     case allWheelDrive
-    case awd = "All-Wheel Drive"
+    //case awd = "All-Wheel Drive"
 }
 
 struct Vehicle {
@@ -20,15 +20,16 @@ struct Vehicle {
     let driveTrain: DriveTrain
 }
 
-let fourRunner = Vehicle(information: .init(Make: "Toyota", Model: "4 Runner", Year: 2014, LicensePlate: "55AA22", driveTrain: .allWheelDrive))
 
-let corolla = Vehicle(information: .init(Make: "Toyota", Model: "Corolla", Year: 2021, LicensePlate: "ABC1EFG", driveTrain: .frontWheelDrive))
+let fourRunner = Vehicle(information: .init(Make: "Toyota", Model: "4 Runner", Year: 2014, LicensePlate: "55AA22"), driveTrain: .allWheelDrive)
 
-let landCruiser = Vehicle(information: .init(Make: "Toyota", Model: "LandCruiser", Year: 2023, LicensePlate: "GR4NDM01", driveTrain: .awd))
+let corolla = Vehicle(information: .init(Make: "Toyota", Model: "Corolla", Year: 2021, LicensePlate: "ABC1EFG"), driveTrain: .frontWheelDrive)
 
-let deForester = Vehicle(information: .init(Make: "Subaru", Model: "DeForester", Year: 2021, LicensePlate: "YG3YBXD", driveTrain: .awd))
+//let landCruiser = Vehicle(information: .init(Make: "Toyota", Model: "LandCruiser", Year: 2023, LicensePlate: "GR4NDM01"), driveTrain: .awd)
+//
+//let deForester = Vehicle(information: .init(Make: "Subaru", Model: "DeForester", Year: 2021, LicensePlate: "YG3YBXD"), driveTrain: .awd)
 
-//let carArray = [fourRunner, corolla]
+let carArray = [fourRunner, corolla]
 
-//print(carArray)
+print(carArray)
 
